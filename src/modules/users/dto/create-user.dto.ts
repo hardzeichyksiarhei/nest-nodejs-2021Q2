a0f152198ca8c85197f73479cc6ee604fb7c78e5
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-07-03 23:30:34 +0300

    feat: add user create dto

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+import { IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  readonly name: string;
+
+  @IsNotEmpty()
+  readonly login: string;
+
+  @MinLength(6)
+  readonly password: string;
+}
