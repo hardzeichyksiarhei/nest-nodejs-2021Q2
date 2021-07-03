9fe8decc838687b969c9c3ba9fe3bc8539498e84
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-07-04 01:36:43 +0300

    feat: add column module

---
+++ src/modules/columns/column.model.ts
@@ -0,0 +1,23 @@
+import { v4 as uuid } from 'uuid';
+
+import { IColumn, IBaseColumn } from './column.interface';
+
+export class Column implements IColumn {
+  id: string;
+  title: string;
+  order: number;
+
+  constructor({
+    id = uuid(),
+    title = 'COLUMN',
+    order = -1,
+  }: Partial<IColumn> = {}) {
+    this.id = id;
+    this.title = title;
+    this.order = order;
+  }
+
+  static async create(payload: IBaseColumn): Promise<IColumn> {
+    return new Column(payload);
+  }
+}
